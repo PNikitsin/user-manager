4e9c40695bdf36abfaea8fefe248b5a95a49cd38
Pavel Nikitsin <pnikitinbox@gmail.com>
2024-06-09 02:28:32 +0300

    Added migrations.

--- UserManager/Data/ApplicationDbContext.cs
+++ UserManager/Data/ApplicationDbContext.cs
@@ -7,6 +7,6 @@ namespace UserManager.Data
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options) { Database.EnsureCreated(); }
+            : base(options) { }
     }
 }
